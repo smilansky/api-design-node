711af5274e07b3821d1dc6cfd53ebba0682a50a4
Daniel Smilansky <dsmilansky@gmail.com>
2016-03-26 20:43:06 -0700

    step-2

--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ var _ = require('lodash');
 // with in client as a static resource
 // also, it will server the index.html on the
 // root of that directory on a GET to '/'
+
 app.use(express.static('client'));
 
 // body parser makes it possible to post JSON to the server
@@ -18,10 +19,61 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 
+
+
 var lions = [];
 var id = 0;
 
 // TODO: make the REST routes to perform CRUD on lions
+app.get('/lions', function(req, res){
+  res.json(lions);
+});
+
+app.get('/lions/:id', function(req, res){
+  var lion = _.find(lions, {id: req.params.id});
+  res.json(lion || {});
+});
+
+app.post('/lions', function(req, res){
+  var lion = req.body;
+  id++;
+  lion.id = id + '';
+  lions.push(lion);
+
+  res.json(lion);
+});
+
+app.put('/lions/:id', function(req, res){
+
+  var update = req.body;
+
+  if (update) {
+    delete update.id; 
+  }
+
+  var lion = _.find(lions, {id: req.params.id});
+
+  if (!lions[lion]) {
+    res.send(); 
+  } else {
+    var updatedLion = _.assigns(lions[lion],  update);   
+    res.json(updatedLion);
+  }
+});
+
+app.delete('/lions/:id', function(req, res){
+  var lion = _.findIndex(lions, {id: req.params.id});
+  if (!lions[lion]) {
+    res.send(); 
+  } else {
+    var deletedLion = lions[lion];
+    lions.splice(lion, 1); 
+    res.json(deletedLion)
+  }
+});
+
+
 
 app.listen(3000);
 console.log('on port 3000');
+
