6143aa26e83dbb1e3b1f304a023bb74391832d2e
Daniel Smilansky <dsmilansky@gmail.com>
2016-03-26 17:30:33 -0700

    step-1 complete

--- server.js
+++ server.js
@@ -3,3 +3,22 @@
 // it should then send back jsonData on a GET to /data
 
 var jsonData = {count: 12, message: 'hey'};
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html', function(err) {
+    if (err) {
+      res.status(500).send(err); 
+    } 
+  });
+});
+
+app.get('/data', function(req, res){
+  res.json(jsonData);
+});
+
+var port = 3000;
+app.listen(port, function(){
+  console.log('Listening on localhost://' + port);
+});
